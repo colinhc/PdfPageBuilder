27ebb1f419276ec34edf7330d842ce8c4fc4bf3d
CC <noreply@colination.com>
2011-01-23 16:56:18 -0800

    add getopt and pydoc

--- extract_pdf_cover.py
+++ extract_pdf_cover.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python2.6
 
+import getopt
 import filecmp
 import fnmatch
 import logging
@@ -15,11 +16,29 @@ kPdfRipImage_bin = os.path.join(
 
 
 def FindFiles(ext, dir):
+  """Finds files by the extension in the dir.
+
+  Args:
+    ext: str, file extension.
+    dir: str, directory to look.
+
+  Return:
+    a list of files match the given extension.
+  """
   pattern = '*.%s' % ext
   return [os.path.join(dir, f) for f in os.listdir(dir) if fnmatch.fnmatch(f, pattern)]
 
 
-def ExtractPdfCover(f, format_type):
+def ExtractPdfCover(f, format_type='jpeg'):
+  """Extracts cover page from the pdf file.
+
+  Args:
+    f: str, pdf file.
+    format_type: format to extract the cover page to. 'jpeg', 'png'.
+
+  Return:
+    the directory contains the image.
+  """
   options = 'EXTRACT=pages PAGE_START=1 PAGE_END=1 FORMAT=%s' % format_type
   cmd = '%s "%s" %s' % (kPdfRipImage_bin, f, options)
   subprocess.check_call(shlex.split(cmd))
@@ -27,16 +46,37 @@ def ExtractPdfCover(f, format_type):
 
 
 def ShrinkJpeg(input_dir, dest_file, size='280x320'):
+  """Shrings the all jpeg files in the input_file to size to the dest_file.
+
+  Args:
+    input_dir: str, source image directory.
+    dest_file: str, output filename.
+    size: str, size to shrink to.
+
+  Return:
+    the output file location.
+  """
   cmd = 'convert "%s"/* -resize %s "%s"' % (input_dir, size, dest_file)
   subprocess.check_call(shlex.split(cmd))
   return dest_file
 
 
 def Cleanup(dir_to_clean):
+  """Directory to remove.
+
+  Args:
+    dir_to_clean: str, directory to remove.
+  """
   shutil.rmtree(dir_to_clean)
 
 
 def Publish(srcfile_list, dest_dir):
+  """Publishes the files to destination directory.
+
+  Args:
+    srcfile_list: list[str], the list of files to copy to the output directory.
+    dest_dir: str, the output directory.
+  """
   for f in srcfile_list:
     try:
       logging.info('Publishing file %s to %s' % (f, dest_dir))
@@ -46,11 +86,18 @@ def Publish(srcfile_list, dest_dir):
         os.remove(f)
 
 
-def Run(extension, data_path, publish_path):
+def Run(data_path, publish_path, extension='pdf'):
+  """Starts the conversion process.
+
+  Args:
+    data_path: str, pdf directory to process.
+    publish_path: str, output directory to store the pdf and cover image files.
+    extension: str, 'pdf'.
+  """
   f_list = FindFiles(extension, data_path)
   for i in f_list:
     try:
-      out_dir = ExtractPdfCover(i, 'jpeg')
+      out_dir = ExtractPdfCover(i)
       out_file = ShrinkJpeg(out_dir, '%s.jpg' % i)
       Publish([i, out_file], publish_path)
     except subprocess.CalledProcessError:
@@ -59,8 +106,40 @@ def Run(extension, data_path, publish_path):
       Cleanup(out_dir)
 
 
+def Usage():
+  """Prints usage."""
+  print ('Usage: extract_pdf_cover.py\n  -h --help\n  '
+         '--ext [extension (default "pdf")]\n  --src_path [src_directory]\n  '
+         '--dest_path [publish_directory]\n')
+
+
 def main(argv):
-  Run(argv[1], argv[2], argv[3])
+  options, remainder = getopt.gnu_getopt(
+      argv[1:], 'h', ['help', 'ext', 'src_path=', 'dest_path='])
+
+  ext = None
+  src_path = None
+  dest_path = None
+
+  if not options:
+    Usage()
+    sys.exit(0)
+
+  for opt, value in options:
+    if opt in ('-h', '--help'):
+      Usage()
+      sys.exit(0)
+    elif opt == '--ext':
+      ext = value
+    elif opt == '--src_path':
+      src_path = value
+    elif opt == '--dest_path':
+      dest_path = value
+
+  if ext is None:
+    Run(src_path, dest_path)
+  else:
+    Run(src_path, dest_path, ext)
 
 
 if __name__ == '__main__':
